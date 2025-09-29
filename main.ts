fbebd6dfedb909400050f18d807c2a236d0354a3
okrandomstupidperson <ok.randomstupidperson@gmail.com>
2025-09-29 07:40:22 +0100

    Update pxt.json, main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
-})
+namespace servoControl {
+
+    /**
+     * Set servo angle on selected pin
+     * @param pin micro:bit pin where servo is connected
+     * @param angle angle from 0 to 180 degrees
+     */
+    //% block="set servo on pin %pin|to angle %angle"
+    //% angle.min=0 angle.max=180
+    //% weight=100
+    export function setServoAngle(pin: AnalogPin, angle: number): void {
+        pins.servoWritePin(pin, angle)
+    }
+}
